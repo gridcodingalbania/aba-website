5438c787d1efab312f16c25807e6c53b1ed2ee69
Greit <greitopingari@gmail.com>
2021-12-20 11:41:54 +0100

    project creation

---
+++ Aba/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure--@vab%a7&1wvhm6*bwl4b69*b7k76#+7cv12tp!g@t(t$6)4^s'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
